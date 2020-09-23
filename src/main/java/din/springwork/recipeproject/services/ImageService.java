313b7a0542c60df55acef000b3bcfd02ec9b4e25
dinpwr <58038112+dinpwr@users.noreply.github.com>
2020-09-23 17:45:20 +0300

    Added image upload to DB and made get to download it from DB and show on recipe show page

---
+++ src/main/java/din/springwork/recipeproject/services/ImageService.java
@@ -0,0 +1,8 @@
+package din.springwork.recipeproject.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long id, MultipartFile file);
+}
